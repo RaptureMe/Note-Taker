50a7a5cd24fc55e7de4458a73c5132f5352ee264
RaptureMe <arosu4u@gmail.com>
2023-09-09 11:08:06 -0700

    added routes

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const PORT = 3001;
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
